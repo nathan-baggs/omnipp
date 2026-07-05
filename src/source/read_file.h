23ef5145de01c21b54e93fbbbfa0f02c84b613d2
Nathan Baggs <hello@nathanbaggs.com>
2026-07-05 13:24:53 +0100

    Fix Read source to handle large files

--- src/source/read_file.h
+++ src/source/read_file.h
@@ -2,6 +2,7 @@
 
 #include <cstddef>
 #include <expected>
+#include <memory>
 #include <ranges>
 #include <string>
 
@@ -17,13 +18,16 @@
 #include "utils/auto_release.h"
 #include "utils/fd_closer.h"
 
+using namespace std::literals;
+
 namespace om::source
 {
 
 namespace impl
 {
-constexpr auto static_buffer_size = 2zu * 1024zu * 1024zu;
-alignas(static_buffer_size) static auto static_buffer = std::array<std::byte, static_buffer_size>{};
+constexpr auto static_buffer_size = 10zu * 1024zu * 1024zu;
+constexpr auto huge_page_alignment = 2u * 1024u * 1024u;
+alignas(huge_page_alignment) static auto static_buffer = std::array<std::byte, static_buffer_size>{};
 }
 
 struct ReadFile
@@ -45,18 +49,30 @@ struct ReadFile
             return std::unexpected{std::format("failed to statx file: {}", ::strerror(errno))};
         }
 
+        auto dynamic_buffer = std::unique_ptr<std::byte[]>{};
+        auto buffer_span = std::span<std::byte>{};
+
         const auto size = stx.stx_size;
         if (size > impl::static_buffer_size)
         {
-            return std::unexpected(
-                std::format("file too large for this source: {} > {}", size, impl::static_buffer_size));
+            dynamic_buffer.reset(new std::byte[size]);
+            if (!dynamic_buffer)
+            {
+                return std::unexpected("failed to allocate dynamic buffer"s);
+            }
+
+            buffer_span = {dynamic_buffer.get(), size};
+        }
+        else
+        {
+            buffer_span = impl::static_buffer;
         }
 
         auto read_amount = std::size_t{};
         while (read_amount != size)
         {
             const auto read_this_iter =
-                ::pread(fd, std::ranges::data(impl::static_buffer) + read_amount, size - read_amount, read_amount);
+                ::pread(fd, std::ranges::data(buffer_span) + read_amount, size - read_amount, read_amount);
             if (read_this_iter == -1)
             {
                 return std::unexpected(std::format("failed to read: {} [{}/{} bytes]", path, read_amount, size));
@@ -65,7 +81,7 @@ struct ReadFile
             read_amount += read_this_iter;
         }
 
-        return N{}(std::span{std::ranges::data(impl::static_buffer), size});
+        return N{}(std::span{std::ranges::data(buffer_span), size});
     }
 };
 

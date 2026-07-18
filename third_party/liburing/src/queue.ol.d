75ce5768a363cde9493df5396e99be32a5d16eab
Nathan Baggs <hello@nathanbaggs.com>
2026-07-18 14:16:26 +0100

    Vendor liburing

---
+++ third_party/liburing/src/queue.ol.d
@@ -0,0 +1,22 @@
+queue.ol: queue.c ../config-host.h lib.h arch/x86/lib.h syscall.h \
+ include/liburing.h include/liburing/compat.h include/liburing/io_uring.h \
+ include/liburing/io_uring/query.h include/liburing/io_uring/bpf_filter.h \
+ include/liburing/io_uring_version.h include/liburing/barrier.h \
+ arch/x86/syscall.h arch/x86/../syscall-defs.h int_flags.h \
+ include/liburing/sanitize.h include/liburing/io_uring.h
+../config-host.h:
+lib.h:
+arch/x86/lib.h:
+syscall.h:
+include/liburing.h:
+include/liburing/compat.h:
+include/liburing/io_uring.h:
+include/liburing/io_uring/query.h:
+include/liburing/io_uring/bpf_filter.h:
+include/liburing/io_uring_version.h:
+include/liburing/barrier.h:
+arch/x86/syscall.h:
+arch/x86/../syscall-defs.h:
+int_flags.h:
+include/liburing/sanitize.h:
+include/liburing/io_uring.h:
---
+++ third_party/liburing/src/syscall.ol.d
@@ -0,0 +1,16 @@
+syscall.ol: syscall.c ../config-host.h syscall.h include/liburing.h \
+ include/liburing/compat.h include/liburing/io_uring.h \
+ include/liburing/io_uring/query.h include/liburing/io_uring/bpf_filter.h \
+ include/liburing/io_uring_version.h include/liburing/barrier.h \
+ arch/x86/syscall.h arch/x86/../syscall-defs.h
+../config-host.h:
+syscall.h:
+include/liburing.h:
+include/liburing/compat.h:
+include/liburing/io_uring.h:
+include/liburing/io_uring/query.h:
+include/liburing/io_uring/bpf_filter.h:
+include/liburing/io_uring_version.h:
+include/liburing/barrier.h:
+arch/x86/syscall.h:
+arch/x86/../syscall-defs.h:
